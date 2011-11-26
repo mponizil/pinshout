6ceb5ed6e111d5d2d3fc9faa03501b6fd337ec92
mponizil <misha.ponizil@gmail.com>
2011-11-26 17:03:39 -0500

    fix db i hope

--- settings.py
+++ settings.py
@@ -12,14 +12,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pinshout.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(os.path.dirname(__file__), 'pinshout.db')
+   }
 }
 
 # Local time zone for this installation. Choices can be found here:
